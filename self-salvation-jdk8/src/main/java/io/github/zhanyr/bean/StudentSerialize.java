89c36a2b9fbc8364d4d53725c6177f4371501f59
zhanyr <1023249899@qq.com>
2017-02-15 22:11:39 +0800

    student serialiazable

---
+++ self-salvation-jdk8/src/main/java/io/github/zhanyr/bean/StudentSerialize.java
@@ -0,0 +1,19 @@
+package io.github.zhanyr.bean;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.Serializable;
+
+/**
+ * @Author zhanyr
+ * @Create 2017-02-13 3:38 PM
+ * @Desc
+ */
+public class StudentSerialize implements Serializable{
+    private static final Logger LOGGER = LoggerFactory.getLogger(StudentSerialize.class);
+
+
+
+
+}
